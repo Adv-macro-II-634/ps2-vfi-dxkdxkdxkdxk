c455b16fcf094650e120739e45a8719553c32bb6
XiangkunDai <xiangkunbendai@gmail.com>
2019-09-23 14:49:16 -0400

    updated

--- PS2.R
+++ PS2.R
@@ -2,7 +2,8 @@ install.packages('pracma')
 library(pracma)
 install.packages('zoo')
 library(zoo)
-
+library(ggplot2)
+############################ Q1 - Q3
 alpha = .35
 beta = .99
 sigma = 2
@@ -13,14 +14,15 @@ kbar = (alpha*abar/(rbar))**(1/(1-alpha))
 
 klow = 0.01*kbar
 khigh = 1.99*kbar
-ks = 100
+ks = 51
 ktod = data.frame(linspace(klow, khigh, ks))
-ktom = data.frame(t(ktod))
+#ktom = data.frame(t(ktom))
 
-ktod_mat = data.frame(rep(ktod,ks))
-ktom_mat = data.frame(t(ktod_mat))
-ktod_mat = as.matrix(ktod_mat)
-ktom_mat = as.matrix(ktom_mat)
+k_mat = data.frame(rep(ktod,ks))
+#ktod_m = data.frame(t(ktom_m))
+#ktom_mat = as.matrix(ktom_m) 
+#ktod_mat = as.matrix(ktod_m)
+
 a = c(1.1, 0.678)
 pmat = matrix(c(0.977,1-0.926,1-0.977,0.926),nrow=2,ncol=2)
 
@@ -35,17 +37,40 @@ Sys.time()
 while (dis > tol){
   VF = matrix(0,nrow=2,ncol=ks)
   for (i in 1:2){
-    cons = a[i]*ktod_mat^alpha + (1-delta)*ktod_mat - ktom_mat
+    cons = a[i]*k_mat^alpha + (1-delta)*k_mat - t(k_mat)
     ret = cons^(1-sigma)/(1-sigma)
     ret[cons<0] = -Inf
     p = matrix(rep(pmat[i,], ks), ks, byrow = T)
+    value_mat =  ret + beta*p%*%VF_guess
+    VF[i,] = apply(value_mat,1, max) 
     
-    value_mat =  ret + beta*p%*%VF_guess[i,]
-    VF[i,] = apply(value_mat,2, max) 
-    
-    PF[i,] = ktod[apply(value_mat, 2, which.max),]
+    PF[i,] = ktod[apply(value_mat, 1, which.max),]
   }
   dis = abs(max(VF - VF_guess))
   VF_guess = VF
 }
 Sys.time()
+
+plot = data.frame(t(VF))
+plot$k = ktod$linspace.klow..khigh..ks.
+names(plot)[1:2] = c('high','low')
+ggplot() + 
+  geom_line(data = plot[,c(1,3)], aes(x=k, y=high), color='red') +
+  ggtitle("Value Function") +
+  xlab("k")+
+  ylab("value function") + 
+  geom_line(data = plot[,c(2,3)], aes(x=k, y=low), color='blue') 
+  
+plot = data.frame(t(PF))
+plot$k = ktod$linspace.klow..khigh..ks.
+
+names(plot)[1:2] = c('high','low')
+ggplot() + 
+  geom_line(data = plot[,c(1,3)], aes(x=k, y=high), color='red') +
+  ggtitle("Value Function") +
+  xlab("k")+
+  ylab("value function") + 
+  geom_line(data = plot[,c(2,3)], aes(x=k, y=low), color='blue') 
+
+################ Q4
+
